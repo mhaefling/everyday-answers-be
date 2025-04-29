cec288bf6ba14f905b078336f82b4f87cc243a9c
Matt Haefling <m.haefling@protonmail.com>
2025-04-29 08:19:07 -0600

    feature/created_user removed update action from routes file until its needed.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create, :update] do
+      resources :users, only: [:create] do
       end
     end
   end
